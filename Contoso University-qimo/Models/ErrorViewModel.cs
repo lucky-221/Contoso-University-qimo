50b7db49966065827f381ccf4dbcc6bbb51f5b43
Admin <Admin@LAPTOP-E38PGBBF>
2022-06-13 13:06:24 +0800

    添加项目文件。

---
+++ Contoso University-qimo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Contoso_University_qimo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
